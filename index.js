18e89b391d6d9549675079bfb3807abf4477ebec
Arvin Xu <arvinx@foxmail.com>
2018-02-18 19:33:58 +0800

    :memo: Clean comments

--- index.js
+++ index.js
@@ -70,21 +70,21 @@ module.exports = {
         ':busts_in_silhouette:',
         ':card_file_box:',
       ],
-    ], // 首行验证类型为 gitmoji 的内容
-    'body-leading-blank': [2, 'always'], // 内容以空行开始
-    'footer-leading-blank': [2, 'always'], // 结尾以空行开始
-    'header-max-length': [2, 'always', 72], // 标题最大长度 72 个字符
-    'scope-case': [2, 'always', 'lower-case'], // Scope 永远小写
-    'subject-case': [2, 'always', ['sentence-case']], // 标题必须以大写字母开头
-    'subject-empty': [2, 'never'], // 不允许标题空着
-    'subject-full-stop': [2, 'never', ['.']], // 不允许使用句号
-    'type-case': [2, 'always', 'lower-case'], // type 必须小写
-    'type-empty': [2, 'never'], // type 不能为空
+    ],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', ['sentence-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', ['.']],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
   },
   parserPreset: {
     parserOpts: {
       headerPattern: /^(:\w*:)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
       headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
-    }, // gitmoji commit 提取表达式 for commitlint
-  }, // 为 gitmoji 风格提供单独的解析器
+    },
+  },
 };
