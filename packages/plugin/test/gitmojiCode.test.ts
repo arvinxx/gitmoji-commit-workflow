db8e33b0b55af6cf404cc5151416bc4f0cdd9a52
arvinxx <arvinx@foxmail.com>
2021-01-18 03:00:35 +0800

    :white_check_mark: test(plugin): update test file

--- packages/plugin/test/gitmojiCode.test.ts
+++ packages/plugin/test/gitmojiCode.test.ts
@@ -1,12 +1,12 @@
 import { existsSync, unlinkSync, copyFileSync } from 'fs';
 import { join } from 'path';
 
-const filePath = join(__dirname, '../src', 'gitmojis.json');
-const targetPath = join(__dirname, '../src', 'gitmojis-for-test.json');
+const targetPath = join(__dirname, '../src', 'gitmojis.json');
+const testFilePath = join(__dirname, 'gitmojis-for-test.json');
 
 describe('without gitmoji json', () => {
   it('just return result', async () => {
-    if (!existsSync(filePath)) copyFileSync(targetPath, filePath);
+    if (!existsSync(targetPath)) copyFileSync(testFilePath, targetPath);
 
     const gitmojiCodes = await import('../src/gitmojiCode');
 
@@ -15,7 +15,7 @@ describe('without gitmoji json', () => {
 
   it('will download gitmoji json and write to file', async () => {
     // 如果存在 gitmoji 直接删除
-    if (existsSync(filePath)) unlinkSync(filePath);
+    if (existsSync(targetPath)) unlinkSync(targetPath);
     const gitmojiCodes = await import('../src/gitmojiCode');
     expect(gitmojiCodes.default).toBeInstanceOf(Array);
   });
