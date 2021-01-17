1fa0af3b28b84a5a95d525da75b043cf5c175b18
arvinxx <arvinx@foxmail.com>
2021-01-18 03:12:37 +0800

    :wrench: chore(plugin): update publish config

--- packages/plugin/.fatherrc.ts
+++ packages/plugin/.fatherrc.ts
@@ -1,4 +1,5 @@
 module.exports = {
   entry: 'src/index.ts',
   cjs: 'babel',
+  target: 'node',
 };
--- packages/plugin/src/rule.ts
+++ packages/plugin/src/rule.ts
@@ -1,5 +1,5 @@
-import { Rule } from "@commitlint/types";
-import gitmojiCodes from "./gitmojiCode";
+import { Rule } from '@commitlint/types';
+import gitmojiCodes from './gitmojiCode';
 
 const emoji: Rule = (parsed) => {
   const { raw } = parsed;
@@ -8,13 +8,13 @@ const emoji: Rule = (parsed) => {
   const result = regex.exec(raw);
 
   let pass;
-  let errorMsg = "passed";
+  let errorMsg = 'passed';
 
   // if don't has emoji
   if (!result) {
     pass = false;
     errorMsg =
-      "Your commit should start with gitmoji code,please check the emoji code on https://gitmoji.dev/.";
+      'Your commit should start with gitmoji code,please check the emoji code on https://gitmoji.dev/.';
   } else {
     const emojiCode = result[1];
 
@@ -27,4 +27,4 @@ const emoji: Rule = (parsed) => {
   return [pass, errorMsg];
 };
 
-export = emoji;
+export default emoji;
