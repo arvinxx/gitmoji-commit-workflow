d585ec4ba2c7a277aff89d8a1b44d68e50afb226
arvinxx <arvinx@foxmail.com>
2021-01-19 03:22:18 +0800

    v1.2.0

---
+++ packages/changelog/.changelogrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  scopeDisplayName: {
+    changelog: 'conventional-changelog-gitmoji-config',
+    config: 'commitlint-config-gitmoji',
+    'parser-opts': '@gitmoji/parser-opts',
+    'commit-type': '@gitmoji/commit-types',
+    'commit-types': '@gitmoji/commit-types',
+    '*': 'Other',
+  },
+  displayTypes: ['feat', 'fix', 'styles', 'pref'],
+};
