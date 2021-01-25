06a6ed3a154608e37f95fe526b5b5c92c2494211
arvinxx <arvinx@foxmail.com>
2021-01-25 19:05:20 +0800

    :wrench: chore(ci): fix test ci config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const baseConfig = require('./jest.config.base');
 module.exports = {
   ...baseConfig,
 
-  projects: ['<rootDir>/packages/*/jest.config.ts'],
+  projects: ['<rootDir>/packages/*/jest.config.js'],
   collectCoverageFrom: ['<rootDir>/packages/*/src/**/*.ts'],
   coverageDirectory: '<rootDir>/coverage/',
 };
