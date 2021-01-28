62ec91636da11b2e93e3290379cb5a18d445bb44
arvinxx <arvinx@foxmail.com>
2021-01-29 06:27:54 +0800

    :wrench: chore(ci): add changelog config

--- .changelogrc.js
+++ .changelogrc.js
@@ -2,6 +2,7 @@ module.exports = {
   scopeDisplayName: {
     changelog: 'conventional-changelog-gitmoji-config',
     config: 'commitlint-config-gitmoji',
+    'config-monorepo': 'commitlint-config-gitmoji-monorepo',
     'parser-opts': '@gitmoji/parser-opts',
     'commit-type': '@gitmoji/commit-types',
     'commit-types': '@gitmoji/commit-types',
