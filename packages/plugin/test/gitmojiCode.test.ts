943a8564c8920eb9e92fffc86073d9842a556e68
arvinxx <arvinx@foxmail.com>
2021-01-30 23:49:18 +0800

    :white_check_mark: test(plugin): add catch error test

--- packages/plugin/test/gitmojiCode.test.ts
+++ packages/plugin/test/gitmojiCode.test.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 const targetPath = join(__dirname, '../src', 'gitmojis.json');
 const testFilePath = join(__dirname, 'gitmojis-for-test.json');
 
-describe('without gitmoji json', () => {
+describe('gitmojiCodes work well', () => {
   it('just return result', async () => {
     if (!existsSync(targetPath)) copyFileSync(testFilePath, targetPath);
 
@@ -13,10 +13,36 @@ describe('without gitmoji json', () => {
     expect(gitmojiCodes.default).toBeInstanceOf(Array);
   });
 
-  it('will download gitmoji json and write to file', async () => {
+  it('will download gitmoji json and write to file without gitmoji json', async () => {
     // 如果存在 gitmoji 直接删除
     if (existsSync(targetPath)) unlinkSync(targetPath);
     const gitmojiCodes = await import('../src/gitmojiCode');
     expect(gitmojiCodes.default).toBeInstanceOf(Array);
+  });
+});
+
+describe('gitmojiCodes throw Error', () => {
+  beforeEach(() => {
+    jest.mock('child_process', () => {
+      return {
+        execFileSync: () => {
+          throw Error('');
+        },
+      };
+    });
+  });
+
+  it('will throw Error when fail to fetch gitmojis.json', async () => {
+    if (existsSync(targetPath)) unlinkSync(targetPath);
+    try {
+      const gitmojiCodes = await import('../src/gitmojiCode');
+      expect(gitmojiCodes.default).toBeInstanceOf(Array);
+    } catch (e) {
+      expect(e).toEqual(
+        Error(
+          'Failed to fetch gitmoji JSON, please refer to https://github.com/arvinxx/gitmoji-commit-workflow/tree/master/packages/plugin#fetch-error for help.',
+        ),
+      );
+    }
   });
 });
