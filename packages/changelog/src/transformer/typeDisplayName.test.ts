c7dfa7857d859c05b1c15bf072882e2b6585ba97
arvinxx <arvinx@foxmail.com>
2021-01-31 00:28:41 +0800

    :sparkles: feat(i18n): support switch language

---
+++ packages/changelog/src/transformer/typeDisplayName.test.ts
@@ -0,0 +1,53 @@
+import type { DisplayNameOptions } from './typeDisplayName';
+import { getDisplayName } from './typeDisplayName';
+
+describe('typeDisplayName', () => {
+  it('should return English and emoji by default', () => {
+    expect(getDisplayName('feat')).toEqual('✨ Features');
+    expect(getDisplayName('fix')).toEqual('🐛 Bug Fixes');
+    expect(getDisplayName('perf')).toEqual('⚡ Performance Improvements');
+    expect(getDisplayName('revert')).toEqual('⏪ Reverts');
+    expect(getDisplayName('style')).toEqual('💄 Styles');
+    expect(getDisplayName('docs')).toEqual('📝 Documentation');
+    expect(getDisplayName('refactor')).toEqual('♻ Code Refactoring');
+    expect(getDisplayName('build')).toEqual('👷 Build System');
+    expect(getDisplayName('test')).toEqual('✅ Tests');
+    expect(getDisplayName('ci')).toEqual('🔧 Continuous Integration');
+    expect(getDisplayName('chore')).toEqual('🎫 Chores');
+  });
+
+  it('should return Chinese with { language: "zh-CN" }', () => {
+    const opts: DisplayNameOptions = { language: 'zh-CN' };
+    expect(getDisplayName('feat', opts)).toEqual('✨ 新特性');
+    expect(getDisplayName('fix', opts)).toEqual('🐛 修复');
+    expect(getDisplayName('perf', opts)).toEqual('⚡ 性能优化');
+    expect(getDisplayName('revert', opts)).toEqual('⏪ 回滚');
+    expect(getDisplayName('style', opts)).toEqual('💄 样式');
+    expect(getDisplayName('docs', opts)).toEqual('📝 文档');
+    expect(getDisplayName('refactor', opts)).toEqual('♻ 重构');
+    expect(getDisplayName('build', opts)).toEqual('👷 构建系统');
+    expect(getDisplayName('test', opts)).toEqual('✅ 测试');
+    expect(getDisplayName('ci', opts)).toEqual('🔧 持续集成');
+    expect(getDisplayName('chore', opts)).toEqual('🎫 杂项');
+  });
+
+  it('should return without emoji with { withEmoji: false }', () => {
+    const opts = { withEmoji: false };
+    expect(getDisplayName('feat', opts)).toEqual('Features');
+    expect(getDisplayName('fix', opts)).toEqual('Bug Fixes');
+    expect(getDisplayName('perf', opts)).toEqual('Performance Improvements');
+    expect(getDisplayName('revert', opts)).toEqual('Reverts');
+    expect(getDisplayName('style', opts)).toEqual('Styles');
+    expect(getDisplayName('docs', opts)).toEqual('Documentation');
+    expect(getDisplayName('refactor', opts)).toEqual('Code Refactoring');
+    expect(getDisplayName('build', opts)).toEqual('Build System');
+    expect(getDisplayName('test', opts)).toEqual('Tests');
+    expect(getDisplayName('ci', opts)).toEqual('Continuous Integration');
+    expect(getDisplayName('chore', opts)).toEqual('Chores');
+  });
+
+  it('should should return input when is not valid type', () => {
+    expect(getDisplayName('wip')).toEqual('wip');
+    expect(getDisplayName('aaa')).toEqual('aaa');
+  });
+});
--- packages/changelog/src/transformer/typeDisplayName.ts
+++ packages/changelog/src/transformer/typeDisplayName.ts
@@ -1,32 +1,85 @@
 import type { CommitTypes } from '@gitmoji/commit-types';
 
-export const getDisplayName = (type: CommitTypes | string): string => {
-  switch (type) {
-    case 'feat':
-      return `✨ Features | 新特性`;
-    case 'fix':
-      return `🐛 Bug Fixes | 修复`;
-    case 'perf':
-      return `⚡ Performance Improvements | 性能优化`;
-    case 'revert':
-      return `⏪ Reverts | 回退`;
-    case 'style':
-    case 'styles':
-      return `💄 Styles | 样式`;
-    case 'docs':
-      return `📝 Documentation | 文档`;
-    case 'refactor':
-      return `♻ Code Refactoring | 重构`;
-    case 'build':
-      return `👷‍ Build System`;
-    case 'test':
-      return `✅ Tests | 测试`;
-    case 'ci':
-      return `🔧 Continuous Integration 持续集成`;
-    case 'chore':
-      return `🎫 Chores 杂项`;
-    // 其他都过滤不显示
-    default:
-      return type;
+export interface DisplayNameOptions {
+  withEmoji?: boolean;
+  language?: 'en-US' | 'zh-CN';
+}
+
+interface TypeNameMap {
+  emoji: string;
+  'en-US': string;
+  'zh-CN': string;
+}
+
+const typeMap: Record<Exclude<CommitTypes, 'wip'>, TypeNameMap> = {
+  feat: {
+    emoji: '✨',
+    'en-US': 'Features',
+    'zh-CN': '新特性',
+  },
+  fix: {
+    emoji: '🐛',
+    'en-US': 'Bug Fixes',
+    'zh-CN': '修复',
+  },
+  build: {
+    emoji: '👷',
+    'en-US': 'Build System',
+    'zh-CN': '构建系统',
+  },
+  chore: {
+    emoji: '🎫',
+    'en-US': 'Chores',
+    'zh-CN': '杂项',
+  },
+  ci: {
+    emoji: '🔧',
+    'en-US': 'Continuous Integration',
+    'zh-CN': '持续集成',
+  },
+  docs: {
+    emoji: '📝',
+    'zh-CN': '文档',
+    'en-US': 'Documentation',
+  },
+  test: {
+    emoji: '✅',
+    'zh-CN': '测试',
+    'en-US': 'Tests',
+  },
+  perf: {
+    emoji: '⚡',
+    'en-US': 'Performance Improvements',
+    'zh-CN': '性能优化',
+  },
+  refactor: {
+    emoji: '♻',
+    'en-US': 'Code Refactoring',
+    'zh-CN': '重构',
+  },
+  revert: {
+    emoji: '⏪',
+    'zh-CN': '回滚',
+    'en-US': 'Reverts',
+  },
+  style: {
+    emoji: '💄',
+    'en-US': 'Styles',
+    'zh-CN': '样式',
+  },
+};
+
+export const getDisplayName = (
+  type: CommitTypes | string,
+  options: DisplayNameOptions = {},
+): string => {
+  const { withEmoji = true, language = 'en-US' } = options;
+
+  if (type in typeMap) {
+    const item = typeMap[type];
+    const { emoji } = item;
+    return `${withEmoji ? `${emoji} ` : ''}${item[language]}`;
   }
+
+  return type;
 };
