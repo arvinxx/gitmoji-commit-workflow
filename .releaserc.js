ff2f076acbd728bb1c28e92609737cfc1f5e9355
arvinxx <arvinx@foxmail.com>
2021-03-06 19:43:00 +0800

    :wrench: chore: update release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,33 +1,3 @@
 module.exports = {
-  plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        config: 'conventional-changelog-gitmoji-config',
-      },
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        config: 'conventional-changelog-gitmoji-config',
-      },
-    ],
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-        changelogTitle: '# Changelog',
-      },
-    ],
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message:
-          ':bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
-      },
-    ],
-  ],
+  extends: ['semantic-release-config-gitmoji'],
 };
