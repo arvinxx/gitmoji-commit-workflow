2dff5605bdb8894678aaa91a9356fd3539397b5a
arvinxx <arvinx@foxmail.com>
2021-03-12 11:28:35 +0800

    :wrench: chore: update release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,3 @@
-module.exports = {
-  branches: [
-    'master',
-    { name: 'next', channel: 'next', prerelease: 'rc' },
-    { name: 'beta', prerelease: true },
-  ],
-  extends: ['semantic-release-config-gitmoji'],
-};
+const config = require('semantic-release-config-gitmoji');
+
+module.exports = config;
