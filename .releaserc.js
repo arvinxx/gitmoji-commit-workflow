718fc8a775f488cc801f838426f1de924a3a9433
arvinxx <arvinx@foxmail.com>
2021-03-12 11:32:19 +0800

    :package: chore: update release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,35 @@
-const config = require('semantic-release-config-gitmoji');
-
-module.exports = config;
+module.exports = {
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        config: 'conventional-changelog-gitmoji-config',
+        releaseRules: [{ type: 'build', release: 'patch' }],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        config: 'conventional-changelog-gitmoji-config',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Changelog',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          ':bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
