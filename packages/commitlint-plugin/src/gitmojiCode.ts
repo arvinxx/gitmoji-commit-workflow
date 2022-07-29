cf1788f5be1e3ae2a7e6b7636ea1a7ef3b231e08
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-07-29 16:39:13 -0500

    :zap: chore: Replaced fetching gitmoji.json for dependency

--- packages/commitlint-plugin/src/gitmojiCode.ts
+++ packages/commitlint-plugin/src/gitmojiCode.ts
@@ -1,49 +1,10 @@
-import { join } from 'path';
-import { existsSync, writeFileSync } from 'fs';
-import { config } from 'dotenv';
-
 import toEmoji from 'emoji-name-map';
 
-const isTest = process.env.IS_TEST === '1';
-config(isTest ? { path: join(__dirname, '../test', './test.env') } : {});
-
-// if there is GITMOJI_PATH env, use as local file path
-const gitmojiPath = process.env.GITMOJI_PATH;
-const filePath = gitmojiPath
-  ? join(process.cwd(), gitmojiPath)
-  : // or use gitmoji file
-    join(__dirname, 'gitmojis.json');
-
-// Download gitmojis.json if it doesn't exist yet
-if (!existsSync(filePath)) {
-  const url =
-    process.env.GITMOJI_URL ||
-    'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json';
-  try {
-    // eslint-disable-next-line global-require
-    const result = require('child_process').execFileSync(
-      'curl',
-      ['--silent', '-L', url],
-      {
-        encoding: 'utf8',
-        maxBuffer: Infinity,
-      },
-    );
-
-    writeFileSync(filePath, result);
-  } catch (e) {
-    /* istanbul ignore next */
-    throw Error(
-      'Failed to fetch gitmoji JSON, please refer to https://github.com/arvinxx/gitmoji-commit-workflow/tree/master/packages/commitlint-plugin#fetch-error for help.',
-    );
-  }
-}
 // eslint-disable-next-line import/no-dynamic-require
-const { gitmojis } = require(filePath);
+const { gitmojis } = require('gitmojis');
+
 export const gitmojiCodes: string[] = gitmojis.map((gitmoji) => gitmoji.code);
 
 export const gitmojiUnicode: string[] = gitmojis.map((gitmoji) =>
   toEmoji.get(gitmoji.code),
 );
-
-export const localPath = filePath;
