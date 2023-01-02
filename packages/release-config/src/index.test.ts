bcc24a07fe1264da0183ba4d81c15cc80532610e
arvinxx <arvinx@foxmail.com>
2023-01-02 12:23:55 +0800

    :camera_flash: test: update snapshot

--- packages/release-config/src/index.test.ts
+++ packages/release-config/src/index.test.ts
@@ -1,42 +1,5 @@
 import defaultConfig from './index';
 
 test('get default config', () => {
-  expect(defaultConfig).toEqual({
-    plugins: [
-      [
-        '@semantic-release/commit-analyzer',
-        {
-          config: 'conventional-changelog-gitmoji-config',
-          releaseRules: [
-            { type: 'style', release: 'patch' },
-            { type: 'build', release: 'patch' },
-          ],
-        },
-      ],
-      [
-        '@semantic-release/release-notes-generator',
-        {
-          config: 'conventional-changelog-gitmoji-config',
-        },
-      ],
-      [
-        '@semantic-release/changelog',
-        {
-          changelogFile: 'CHANGELOG.md',
-          changelogTitle: '# Changelog',
-        },
-      ],
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'package.json'],
-          message:
-            // eslint-disable-next-line no-template-curly-in-string
-            ':bookmark: chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
-        },
-      ],
-    ],
-  });
+  expect(defaultConfig).toMatchSnapshot();
 });
