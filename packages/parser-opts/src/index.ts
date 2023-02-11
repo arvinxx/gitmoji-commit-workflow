bc09794524a05ea58b6b41fd421dfbd2dbcb0285
arvinxx <arvinx@foxmail.com>
2023-02-11 13:03:20 +0800

    :sparkles: feat(parser-opts): support UCS-4 unicode

--- packages/parser-opts/src/index.ts
+++ packages/parser-opts/src/index.ts
@@ -1,6 +1,14 @@
+import { gitmojiCodeRegex, gitmojiUnicodeRegex, emojiRegex } from '@gitmoji/gitmoji-regex';
+
+const gitmojiCodeStr = gitmojiCodeRegex.source;
+const gitmojiUnicodeStr = gitmojiUnicodeRegex.source;
+const emojiStr = emojiRegex.source;
+
 export default {
   // Test URL: https://regex101.com/r/gYkG99/1
-  headerPattern:
-    /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))(?:\s\(?(?<ticket>#\d*)\)?)?$/,
+  headerPattern: new RegExp(
+    `^(?:${gitmojiCodeStr}|(?:${gitmojiUnicodeStr})|(?:${emojiStr}))\\s(?<type>\\w*)(?:\\((?<scope>.*)\\))?!?:\\s(?<subject>(?:(?!#).)*(?:(?!\\s).))(?:\\s\\(?(?<ticket>#\\d*)\\)?)?$`,
+  ),
+
   headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
 };
