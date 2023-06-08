29ffd7bab0da53a42bb41f185cc21635cf189713
canisminor1990 <i@canisminor.cc>
2023-06-08 23:49:15 +0800

    ✅ test(changelog): add `finalizeContext` and `transformer` tests

---
+++ packages/changelog/src/finalizeContext/index.test.ts
@@ -0,0 +1,83 @@
+import type { Context } from 'conventional-changelog-writer';
+import { CustomConfig } from '../customConfig';
+import { typeMap } from '../transformer/typeDisplayName';
+
+describe('test transformCommitGroups', () => {
+  const customConfig: CustomConfig = {
+    scopeDisplayName: {
+      '*': 'all',
+    },
+  };
+  const context: Context = {
+    commitGroups: [
+      {
+        title: '✨ Features',
+        commits: [
+          {
+            hash: '1234',
+            subject: 'test commit',
+            scope: 'test',
+            title: '✨ Features',
+            authorName: 'test',
+            authorEmail: 'test@test.com',
+            authorNameEncode: 'test',
+          },
+        ],
+      },
+    ],
+    authors: [],
+  };
+
+  it('should transform commitGroups correctly', () => {
+    const subCommitScope = customConfig?.scopeDisplayName?.['*'] || '';
+    const authors = {};
+    context.commitGroups = context.commitGroups.map((item) => {
+      const subtitle = Object.values(typeMap).find(
+        (i) =>
+          item.title.includes(i['emoji']) ||
+          item.title.includes(i['en-US']) ||
+          item.title.includes(i['zh-CN']),
+      ).subtitle;
+      let group;
+      let commits = item.commits.sort((a, b) => {
+        if (a.scope === subCommitScope && b.scope === subCommitScope) {
+          return 0;
+        } else if (a.scope === subCommitScope) {
+          return 1;
+        } else if (b.scope === subCommitScope) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      commits = commits.map((c, index) => {
+        if (group !== c.scope) {
+          group = c.scope;
+          c.first = true;
+        } else {
+          c.first = false;
+        }
+        if (!commits[index + 1] || group !== commits[index + 1].scope) {
+          c.last = true;
+        } else {
+          c.last = false;
+        }
+        if (c.authorNameEncode && !authors[c.authorNameEncode]) {
+          authors[c.authorNameEncode] = {
+            authorName: c.authorName,
+            authorEmail: c.authorEmail,
+            authorNameEncode: c.authorNameEncode,
+          };
+        }
+        return c;
+      });
+      return {
+        title: item.title,
+        subtitle,
+        commits,
+      };
+    });
+    context.authors = Object.values(authors);
+    expect(context).toMatchSnapshot();
+  });
+});
