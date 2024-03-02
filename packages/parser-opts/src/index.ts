b74936c25297495f818132dd4d267a9adf6c0d90
jebarpg <jebarpg@gmail.com>
2024-03-01 20:51:29 -0800

    🐛 fix(parser-opts): changed \w* to [a-zA-Z-,\/]+ and added new regex101 example

--- packages/parser-opts/src/index.ts
+++ packages/parser-opts/src/index.ts
@@ -5,7 +5,7 @@ const gitmojiUnicodeStr = gitmojiUnicodeRegex.source;
 const emojiStr = emojiRegex.source;
 
 export default {
-  // Test URL: https://regex101.com/r/gYkG99/1
+  // Test URL: https://regex101.com/r/HYr6d3/1
   headerPattern: new RegExp(
     `^(?:${gitmojiCodeStr}|(?:${gitmojiUnicodeStr})|(?:${emojiStr}))\\s(?<type>[a-zA-Z-,\/]+)(?:\\((?<scope>.*)\\))?!?:\\s(?<subject>(?:(?!#).)*(?:(?!\\s).))(?:\\s\\(?(?<ticket>#\\d*)\\)?)?$`,
   ),
