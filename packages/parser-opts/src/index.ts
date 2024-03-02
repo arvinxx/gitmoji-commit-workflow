248eceb8f3c0f71b8add2e2cb818b53c4e0695f6
jebarpg <jebarpg@gmail.com>
2024-03-01 20:35:54 -0800

    fix(parser-opts): changed ?<type>\w* to ?<type>[a-zA-z-,\/]+ to work with - and / i.e. fix-ci ci/cd

--- packages/parser-opts/src/index.ts
+++ packages/parser-opts/src/index.ts
@@ -1,4 +1,4 @@
-import { gitmojiCodeRegex, gitmojiUnicodeRegex, emojiRegex } from '@gitmoji/gitmoji-regex';
+import { emojiRegex, gitmojiCodeRegex, gitmojiUnicodeRegex } from '@gitmoji/gitmoji-regex';
 
 const gitmojiCodeStr = gitmojiCodeRegex.source;
 const gitmojiUnicodeStr = gitmojiUnicodeRegex.source;
@@ -7,7 +7,7 @@ const emojiStr = emojiRegex.source;
 export default {
   // Test URL: https://regex101.com/r/gYkG99/1
   headerPattern: new RegExp(
-    `^(?:${gitmojiCodeStr}|(?:${gitmojiUnicodeStr})|(?:${emojiStr}))\\s(?<type>\\w*)(?:\\((?<scope>.*)\\))?!?:\\s(?<subject>(?:(?!#).)*(?:(?!\\s).))(?:\\s\\(?(?<ticket>#\\d*)\\)?)?$`,
+    `^(?:${gitmojiCodeStr}|(?:${gitmojiUnicodeStr})|(?:${emojiStr}))\\s(?<type>[a-zA-z-,\/]+)(?:\\((?<scope>.*)\\))?!?:\\s(?<subject>(?:(?!#).)*(?:(?!\\s).))(?:\\s\\(?(?<ticket>#\\d*)\\)?)?$`,
   ),
 
   headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
